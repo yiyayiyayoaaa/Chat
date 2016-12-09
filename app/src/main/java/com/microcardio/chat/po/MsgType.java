77137e636a90fa6d5a31c5fcaf1657dceb008709
yiyayiyayoaaa <18272175681@163.com>
2016-12-09 13:06:58 +0800

    重写 聊天界面 ListView的Adapter 添加图片内存缓存

---
+++ app/src/main/java/com/microcardio/chat/po/MsgType.java
@@ -0,0 +1,10 @@
+package com.microcardio.chat.po;
+
+/**
+ * Created by AMOBBS on 2016/12/9.
+ */
+public interface MsgType {
+    int IS_IMG = 0; //是图片
+    int IS_AUDIO = 1; //是音频
+    int IS_OTHER = 2; //其他
+}
